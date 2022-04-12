9fbb193731c0f10d5d2b5f762aec75f7f0f123ac
TobiOgunledun <ogunledunoluwatobi@gmail.com>
2022-04-11 18:07:56 -0700

    print single numbers of base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *main - entry point
+ *Description: print single numbers followed  by new line
+ *Return: 0
+ */
+
+int main (void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a + '0');
+	}
+	putchar('\n');
+return (0);
+}
