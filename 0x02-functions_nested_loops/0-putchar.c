9494afcbbe515092992c2a3864b4fa58b5a0efaa
TobiOgunledun <ogunledunoluwatobi@gmail.com>
2022-04-12 16:22:52 -0700

    print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - entry point
+ *Description: print _putchar followd by newline
+ *Return: 0
+ */
+
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a')[B;
+_putchar('r');
+_putchar('\n');
+
+return (0);
+}
