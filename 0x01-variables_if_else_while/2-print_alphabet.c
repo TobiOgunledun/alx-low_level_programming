be2cf6ff94a1f3fe9532c6bef6d3082775fa3ff9
TobiOgunledun <ogunledunoluwatobi@gmail.com>
2022-04-11 17:40:05 -0700

    alphabet in lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *main - entry point
+ *Description: print alphabet in lower case
+ *Return: 0
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar("\n");
+return (0);
+}
