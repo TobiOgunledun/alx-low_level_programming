66757e38061681d97913267a8abfa8e1373c6596
TobiOgunledun <ogunledunoluwatobi@gmail.com>
2022-04-11 17:56:00 -0700

    alphabet in lowercase

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *main - entry point
+ *Description: print alphabets in lower case followed by new line
+ *Return: 0
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch)
+		}
+		else
+		{
+		}
+	}
+	putchar('\n');
+return (0);
+}
