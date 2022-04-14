a5e86d57319085508327dca2d3492163f6b56bb3
TobiOgunledun <ogunledunoluwatobi@gmail.com>
2022-04-13 18:36:48 -0700

    print absolute values

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *main - check the code
+ *Description: print absolute value
+ *Return: 0
+ */
+
+int _abs(int n)
+{
+	if n < 0>
+	if (n < 0)
+	{
+		return (n * (-1));
+	}
+	else if (n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+	return (0);
+	}
+}
