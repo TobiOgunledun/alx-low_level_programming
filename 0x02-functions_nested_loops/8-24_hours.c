4b197aa098be5c5490a602ed0b92936b3afe4fb3
Tobi <87040784+TobiOgunledun@users.noreply.github.com>
2022-04-13 19:50:58 -0700

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,15 +2,15 @@
 /**
  *jack_bauer - check the code
  *Description: print every minute of the day
- *rETURN: 0
+ *Return: 0
  */
 
-int jack_bauer(void)
+void jack_bauer(void)
 {
 	int a;
 	int b;
 
-FOR (a = 0; a <= 23; a++)
+for (a = 0; a <= 23; a++)
 	{
 		for (b = 0; b <= 5; b++)
 		{
