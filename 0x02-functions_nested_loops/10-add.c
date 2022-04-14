2528db21ad0cb2623a8bd43e327172a5b80309fa
TobiOgunledun <ogunledunoluwatobi@gmail.com>
2022-04-13 19:39:03 -0700

    print addition of two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *add - check the code
+ *Description: prints add two numbers
+ *@i: value
+ *@k: value
+ *Return: 0
+ */
+
+int add(int i, int k)
+{
+	return (i +k);
+}
