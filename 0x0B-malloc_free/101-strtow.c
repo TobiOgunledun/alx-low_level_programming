6e8fde6c7d94bee68178c96e2302de2bf4512293
Tobi <87040784+TobiOgunledun@users.noreply.github.com>
2022-05-06 16:53:52 -0700

    Update 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -7,7 +7,9 @@
  */
 int word_len(char *str);
 int count_words(char *str);
-char **strtow(char *str)
+char **strtow(char *str);
+
+int word_len(char *str)
 {
 	char **ptr;
 	int i, k, len, start, end, j = 0;
