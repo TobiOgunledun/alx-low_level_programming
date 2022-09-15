b58768926a87734641df9e9ba5a027f574d4a539
Tobi <87040784+TobiOgunledun@users.noreply.github.com>
2022-09-15 14:51:28 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,22 +1,10 @@
-#include <unistd.h>
-
-
-
+#include <stdio.h>
 /**
 
  * main - Entry point
-
- *
-
  * Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
-
- *
-
  * Return: Always 0 (Success)
-
  */
-
-
 
 int main(void)
 
