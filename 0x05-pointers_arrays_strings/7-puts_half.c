479831287e2ebaedf22217a223dff6e4dcca9f52
TobiOgunledun <ogunledunoluwatobi@gmail.com>
2022-09-22 07:35:35 -0700

    ointer tasks

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-
-
 /**
  * puts_half - prints the second half of the string
  * @str: the string reference
@@ -11,13 +9,27 @@
 
 
 void puts_half(char *str)
+
 {
+
 	int i;
-	for (i = 0; str[i] != '\0'; i++);
+
+
+
+	for (i = 0; str[i] != '\0'; i++)
+
+		;
+
 	i++;
-	for (i /= 2; str[i] != '\0'; i++)	
+
+	for (i /= 2; str[i] != '\0'; i++)
+
 	{
-	_putchar(str[i]);
-	{
-		_putchar('\n');
+
+		_putchar(str[i]);
+
+	}
+
+	_putchar('\n');
+
 }
