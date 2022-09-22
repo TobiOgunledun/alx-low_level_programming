40eb8dbc2d5a8ed5d4d1952330fe672ea199239f
TobiOgunledun <ogunledunoluwatobi@gmail.com>
2022-09-22 05:59:46 -0700

    pointer task

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,17 +1,10 @@
 #include "main.h"
 
 
-
 /**
-
- * reset_to_98 - updates the value of n to 98
-
+ * reset_to_98 - updates the value of n to 9
  * @n: integer pointer to a variable to be updated
-
- *
-
  * Return: nothing
-
  */
 
 
