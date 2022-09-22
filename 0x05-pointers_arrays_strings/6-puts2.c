2909d308b23974700972cec54bde91cd0f1ef578
TobiOgunledun <ogunledunoluwatobi@gmail.com>
2022-09-22 07:25:57 -0700

    pointer task

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,21 +12,15 @@
 void puts2(char *str)
 
 {
+	int i = 0;
 
-		int i = 0;
-
-
-
-			while (str[i] != '\0')
-
-					{
-
-								if (i % 2 == 0)
-
-											{
-
-															_putchar(str[i]);														
+	while (str[i] != '\0')
+	{
+	if (i % 2 == 0)
+	{
+	_putchar(str[i]);
+	}
 	i++;
 	}
-		_putchar('\n');
+	_putchar('\n');
 }
